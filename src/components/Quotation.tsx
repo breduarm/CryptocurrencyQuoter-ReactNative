23d2722eca6d16a8c76863f4ed7e99f2c1ee5d7d
breduarm <bryan.e.armijos@gmail.com>
2024-06-30 11:00:00 -0500

    Add more information to the Quotation component

--- src/components/Quotation.tsx
+++ src/components/Quotation.tsx
@@ -5,12 +5,35 @@ const Quotation = ({quotation}) => {
   if (Object.keys(quotation).length === 0) return null;
 
   return (
-    <View>
-      <Text>{quotation.price}</Text>
+    <View style={styles.result}>
+      <Text style={styles.text}>
+        <Text style={styles.span}>{quotation.PRICE}</Text>
+      </Text>
+      <Text style={styles.text}>
+        Highest price of the day: {' '}
+        <Text style={styles.span}>{quotation.HIGHDAY}</Text>
+      </Text>
+      <Text style={styles.text}>
+        Lowest price of the day: {' '}
+        <Text style={styles.span}>{quotation.LOWDAY}</Text>
+      </Text>
+      <Text style={styles.text}>
+        Variation on the last hours: {' '}
+        <Text style={styles.span}>{quotation.CHANGEPCT24HOURS} % </Text>
+      </Text>
+      <Text style={styles.text}>
+        Last update: {' '}
+        <Text style={styles.span}>{quotation.LASTUPDATE}</Text>
+      </Text>
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const styles = StyleSheet.create({
+  result: {},
+  text: {},
+  price: {},
+  span: {},
+});
 
 export default Quotation;
